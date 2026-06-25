3e9dc747bff9e19a9ffa498a06a38aebab152ef9
laikacuriosity <t.u.jayasri@gmail.com>
2026-06-25 21:08:15 +0530

    Create visaBins.js

---
+++ server/visaBins.js
@@ -0,0 +1,58 @@
+const visaBins = {
+  // Visa Classic
+  "411111": {
+    issuer: "Visa Test Bank",
+    cardType: "visa_classic"
+  },
+
+  "424242": {
+    issuer: "Demo Bank",
+    cardType: "visa_classic"
+  },
+
+  // Visa Gold
+  "401234": {
+    issuer: "Demo Gold Bank",
+    cardType: "visa_gold"
+  },
+
+  "453978": {
+    issuer: "Demo Gold Bank",
+    cardType: "visa_gold"
+  },
+
+  // Visa Platinum
+  "455673": {
+    issuer: "Demo Platinum Bank",
+    cardType: "visa_platinum"
+  },
+
+  "471658": {
+    issuer: "Demo Platinum Bank",
+    cardType: "visa_platinum"
+  },
+
+  // Visa Signature
+  "401288": {
+    issuer: "Demo Signature Bank",
+    cardType: "visa_signature"
+  },
+
+  "453242": {
+    issuer: "Demo Signature Bank",
+    cardType: "visa_signature"
+  },
+
+  // Visa Infinite
+  "448527": {
+    issuer: "Demo Infinite Bank",
+    cardType: "visa_infinite"
+  },
+
+  "476134": {
+    issuer: "Demo Infinite Bank",
+    cardType: "visa_infinite"
+  }
+};
+
+module.exports = visaBins;
