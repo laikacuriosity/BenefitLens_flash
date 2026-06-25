810a808044f738482ebcb7c31437b58260fbe129
laikacuriosity <t.u.jayasri@gmail.com>
2026-06-25 21:09:18 +0530

    Create card.ts

---
+++ utils/card.ts
@@ -0,0 +1,25 @@
+
+import { CardType } from '../types';
+
+export function validateCardLuhn(cardNumber: string): boolean {
+  const digits = cardNumber.replace(/\D/g, '');
+  if (digits.length < 13 || digits.length > 19) return false;
+  if (digits[0] !== '4') return false; // Visa only for now
+
+  let sum = 0;
+  let isEven = false;
+  
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let digit = parseInt(digits[i]);
+    
+    if (isEven) {
+      digit *= 2;
+      if (digit > 9) digit -= 9;
+    }
+    
+    sum += digit;
+    isEven = !isEven;
+  }
+  
+  return sum % 10 === 0;
+}
